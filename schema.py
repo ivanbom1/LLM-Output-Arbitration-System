ba125894d844a296d1e2800704a0510e61a81325
ivan.bomko <vanya.bomko@gmail.com>
2026-09-18 00:34:34 +0200

    critique report class implemented

--- schema.py
+++ schema.py
@@ -1,18 +1,23 @@
 from enum import Enum
 from pydantic import BaseModel, Field
 
-class Dimension(str, Enum):
+class Dimension(str, Enum): # Specifies which exact critique produced a report 
     ACCURACY = "accuracy"
     LOGIC = "logic"
     COMPLETENESS = "completeness"
     
-class Severity(int, Enum):
+class SeverityLevel(int, Enum): # Classification of an Issue. Each one has a different level depended on the specific problem
     LOW = 1
     MEDIUM = 2
     HIGH = 3
     
-class Issue(BaseModel):
-    quote: str = Field(..., description="Problem part from the original output")
+class Issue(BaseModel): #Issue Object, has a problematic part reference, explanation and class of the issue
+    quote: str = Field(..., description="Problem part from the original output") 
     problem: str = Field(..., description="What's the issue")
-    Severity: Severity
-    
+    Severity: SeverityLevel
+
+class CritiqueReportForm(BaseModel): 
+    dimension: Dimension
+    eval_score: int = Field(...,  ge=1, le=5)
+    issues: list[Issue]
+    confidence: float = Field(..., ge=0, le=1)
